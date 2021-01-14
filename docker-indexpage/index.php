a7f62cda097b36ca1a9ac41bc47791eb8b3fe7d8
root <root@ip-172-31-34-121.ap-south-1.compute.internal>
2021-01-14 06:10:08 +0000

    index page for docker container

---
+++ docker-indexpage/index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <title>nginx webserver</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Hello World! by Muhammad Tabish</h1>
+      <p>This is index page of nginx webserver created by me.</p>
+     <img src="https://github.com/mtabishk/MLOps-Internship/blob/master/badge.jpg?raw=true" alt="Tabish" width="300" height="300">	
+    </div>
+    <div class="container">
+    <pre>
+    <?php
+	echo `/usr/sbin/ifconfig`;
+    ?>
+    </pre>
+    </div>
+  </body>
+</html>
